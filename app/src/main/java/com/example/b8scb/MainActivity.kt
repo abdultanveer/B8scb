42501f3c7cc6b8d4b68b6fa4825bc5e977949980
abdul <abdul.tanveer@gmail.com>
2024-09-25 11:02:38 +0530

    snackbar

--- app/src/main/java/com/example/b8scb/MainActivity.kt
+++ app/src/main/java/com/example/b8scb/MainActivity.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
     fun clickHandler(view: View) {
         //add(10,20)
         Log.i("MainActivity","button clicked")
+        var view = findViewById<ConstraintLayout>(R.id.main)
+       // var snackbar =
+            Snackbar.make(view,"1 archived",Snackbar.LENGTH_LONG)
+                .setAction("undo",{})
+                .show()
     }
 
     fun add(firstNo:Int, secondNo:Int):Int{
