e1e84959cc902332994bde95aa4645c3959d01b8
abdul <abdul.tanveer@gmail.com>
2024-09-25 10:57:40 +0530

    clickhandler

--- app/src/main/java/com/example/b8scb/MainActivity.kt
+++ app/src/main/java/com/example/b8scb/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.b8scb
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
+
+    fun clickHandler(view: View) {
+        //add(10,20)
+        Log.i("MainActivity","button clicked")
+    }
+
+    fun add(firstNo:Int, secondNo:Int):Int{
+        return firstNo + secondNo
+    }
+
 }
