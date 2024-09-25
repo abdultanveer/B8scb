1fc2448c27fe2c0a5df4781b549e4a9f2aca4672
abdul <abdul.tanveer@gmail.com>
2024-09-25 11:07:09 +0530

    get data edittext and set it in textview

--- app/src/main/java/com/example/b8scb/MainActivity.kt
+++ app/src/main/java/com/example/b8scb/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.b8scb
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -11,14 +13,20 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+lateinit var emailEditText: EditText
+lateinit var mainTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        emailEditText = findViewById(R.id.etEmail) //taking handle
+        mainTextView = findViewById(R.id.tvMain)
     }
 
     fun clickHandler(view: View) {
         //add(10,20)
+        var data = emailEditText.text.toString()
+        mainTextView.setText(data)
         Log.i("MainActivity","button clicked")
         var view = findViewById<ConstraintLayout>(R.id.main)
        // var snackbar =
