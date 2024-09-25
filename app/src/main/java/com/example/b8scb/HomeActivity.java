8876f7e00ed303817fc4ba2a990f5fc048cc02f8
abdul <abdul.tanveer@gmail.com>
2024-09-25 13:52:03 +0530

    invoke alarm activity

--- app/src/main/java/com/example/b8scb/HomeActivity.java
+++ app/src/main/java/com/example/b8scb/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.b8scb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -25,6 +28,20 @@ TextView homeView;
        String name = getIntent().getExtras().getString("myname");
        homeView.setText(name);
         //set it onnto a textview
+
+    }
+
+    public void setAlarm(View view) {
+        createAlarm("b9scb",13,52);
+    }
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+       // if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
 
     }
 }
