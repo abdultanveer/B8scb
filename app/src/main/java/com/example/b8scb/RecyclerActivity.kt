af22e9a8665ee859706bcbf03563076f23d1326b
abdul <abdul.tanveer@gmail.com>
2024-09-26 14:23:36 +0530

    roomdb-insert data

--- app/src/main/java/com/example/b8scb/RecyclerActivity.kt
+++ app/src/main/java/com/example/b8scb/RecyclerActivity.kt
@@ -6,10 +6,18 @@ import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.b8scb.database.Item
+import com.example.b8scb.database.ItemDao
+import com.example.b8scb.database.ItemRoomDatabase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class RecyclerActivity : AppCompatActivity() {
     var languages = arrayOf("English","hindi","urdu","kannada","tamil")
     lateinit var recyclerView: RecyclerView
+
+    lateinit var dao: ItemDao
+
 
     lateinit var listView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,11 +28,22 @@ recyclerView.layoutManager = LinearLayoutManager(this)
         var adapter = LangsAdapter(languages)
         recyclerView.adapter = adapter
 
+        var database = ItemRoomDatabase.getDatabase(this)
+        dao = database.itemDao()
+
+
+
 
 
     }
 
     fun insertDb(view: View) {
 
+        GlobalScope.launch {  //launch = coroutine
+            var gItem = Item(11, "fruits", 12.99, 12)
+
+            dao.insert(gItem)
+
+        }
     }
 }
