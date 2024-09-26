eb5daec4fabceec1e094ba1c483ca49d358e2f2f
abdul <abdul.tanveer@gmail.com>
2024-09-26 11:59:06 +0530

    recyclerview-3

--- app/src/main/java/com/example/b8scb/LangsAdapter.kt
+++ app/src/main/java/com/example/b8scb/LangsAdapter.kt
@@ -1,12 +1,40 @@
 package com.example.b8scb
 
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class LangsAdapter:RecyclerView.Adapter<VisitingCardsHolder>() {
+class LangsAdapter(var languages: Array<String>) :RecyclerView.Adapter<LangsAdapter.VisitingCardsHolder>() {
+var TAG = LangsAdapter::class.java.simpleName
+    var noTimesBalaMarket = 0
 
+    class VisitingCardsHolder( visitingCard:View):RecyclerView.ViewHolder(visitingCard) {
+        var tvCard:TextView = visitingCard.findViewById(R.id.tvVisitingcard)
+        init {
+            Log.i("VisitingCardsHolder","sangeetha received card from bala")
+        }
 
-    class VisitingCardsHolder(visitingCard:View):RecyclerView.ViewHolder(visitingCard) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VisitingCardsHolder {
+        noTimesBalaMarket++
+
+        Log.i(TAG,"bala --bought a visiting card from market"+noTimesBalaMarket)
+        var vistingCard = LayoutInflater.from(parent.context).inflate(R.layout.visiting_card,parent,false)
+        return VisitingCardsHolder(vistingCard)
+    }
+
+    override fun getItemCount(): Int {
+        Log.i(TAG,"masthan is counting")
+        return languages.size
+    }
+
+    override fun onBindViewHolder(vholder: VisitingCardsHolder, position: Int) {
+        Log.i(TAG,"ismail is writing on the vcard -"+languages.get(position))
+        vholder.tvCard.setText(languages.get(position))
 
     }
 }
--- app/src/main/java/com/example/b8scb/RecyclerActivity.kt
+++ app/src/main/java/com/example/b8scb/RecyclerActivity.kt
@@ -1,20 +1,23 @@
 package com.example.b8scb
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.ListView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerActivity : AppCompatActivity() {
     var languages = arrayOf("English","hindi","urdu","kannada","tamil")
+    lateinit var recyclerView: RecyclerView
 
     lateinit var listView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler)
+        recyclerView = findViewById(R.id.recyclerview)
+recyclerView.layoutManager = LinearLayoutManager(this)
+        var adapter = LangsAdapter(languages)
+        recyclerView.adapter = adapter
 
 
 
