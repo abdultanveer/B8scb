23ccf923c5e6745947c8b5097ffc702325756450
abdul <abdul.tanveer@gmail.com>
2024-09-26 11:46:02 +0530

    recyclerview-2

--- app/src/main/java/com/example/b8scb/LangsAdapter.kt
+++ app/src/main/java/com/example/b8scb/LangsAdapter.kt
@@ -1,4 +1,12 @@
 package com.example.b8scb
 
-class LangsAdapter {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class LangsAdapter:RecyclerView.Adapter<VisitingCardsHolder>() {
+
+
+    class VisitingCardsHolder(visitingCard:View):RecyclerView.ViewHolder(visitingCard) {
+
+    }
 }
