f3af5c74b0ca0ba4e44e5c428a03cba56411e181
abdul <abdul.tanveer@gmail.com>
2024-09-27 14:52:45 +0530

    put parsed json recyclerview

--- app/src/main/java/com/example/b8scb/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/b8scb/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.b8scb.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
+    lateinit var dashboardViewModel:DashboardViewModel
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,17 +24,30 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
+         dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+       /*// val textView: TextView = binding.textDashboard
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
         return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        dashboardViewModel.getMarsPhotos()
+        var adapter = WordListAdapter()
+        binding.recyclerView2.layoutManager =  LinearLayoutManager(context)
+        binding.recyclerView2.adapter = adapter
+        dashboardViewModel.allWords.observe(viewLifecycleOwner) { listMarsPhotos ->
+            // Update the cached copy of the words in the adapter.
+            listMarsPhotos.let { adapter.submitList(it) }
+        }
     }
 
     override fun onDestroyView() {
